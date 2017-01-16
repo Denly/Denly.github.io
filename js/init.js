2b7a8579ed6239b01e46ad1b58522a8a4482ced8
Denly <007623@gmail.com>
2017-01-16 18:24:47 -0500

    ux ui done

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy({scrollOffset: 0});
-
+    $('.materialboxed').materialbox();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
